57567d0e12e6b839eb0e534172048b7ec17ef98b
Art Santana <Art Santana>
2017-09-01 10:45:34 -0400

    newest changes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,51 @@
+
+// link the homies!
+
+var homies = require("../data/homies");
+
+module.exports = function(app) { // now get routes
+  app.get("/api/homies", function(req, res) {
+    res.json(homies);
+    });
+
+// now post routes
+app.post("/api/homies", function(req, res) {
+    
+    // get the best matches to find a new homie
+
+  var newBff = {
+    name: "",
+    photo: "",
+    bffPts: 1000
+  };
+
+// // post/parse the submitted homie 
+  var homieData = req.body;
+  var homieScore = homieData.scores;
+  var totalPts = 0;
+
+  // write for loop to run through all the homies in database
+  for (var i = 0; i < homies.length; i++) {
+    console.log(homies[i].name);
+    totalPts = 0;
+  
+ // then for loop of all the points of da homies
+    for (var j = 0; j < homies[i].scores[j]; j++) {
+
+      ///  then calculate the points
+      totalPts += Math.abs(parseInt(homieScore[j]) - parseInt(homies[i].scores[j]));
+   
+   if (totalPts <= newBff.bffPts) {
+    newBff.name = homies[i].name;
+    newBff.photo = homies[i].photo;
+    newBff.bffPts = totalPts;
+   }   
+    }
+  }
+// save the homie's into the homie's DB
+  homies.push(homieData);
+
+  res.json(newBff);
+
+});
+}
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+// depencies
+
+var path = require("path");
+
+module.exports = function(app) {
+
+
+	
+	app.get("/suver", function(req, res){
+		res.sendFile(path.join(__dirname, "/../public/survey.html"));
+	});
+
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/home.html"));
+	});
+};
